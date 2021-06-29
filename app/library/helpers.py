23c0ffacbba87fa1ce9c8dcfd67e4f51c9f79929
matiasdamelio <matiasdamelio@gmail.com>
2021-06-29 16:51:11 -0300

    Add templates

---
+++ app/library/helpers.py
@@ -0,0 +1,12 @@
+import os
+import markdown
+
+
+def openfile(filename):
+    filepath = os.path.join("app/pages/", filename)
+    with open(filepath, "r", encoding="utf-8") as input_file:
+        text = input_file.read()
+
+    html = markdown.markdown(text)
+    data = {"text": html}
+    return data
--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from app.routers import unsplash, twoforms, accordion
+from .library.helpers import *
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-def test():
-    return {"message": "Test AlgoBalanz"}
+app.include_router(unsplash.router)
+app.include_router(twoforms.router)
+app.include_router(accordion.router)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    data = openfile("home.md")
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+
+
+@app.get("/page/{page_name}", response_class=HTMLResponse)
+async def page(request: Request, page_name: str):
+    data = openfile(page_name + ".md")
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
